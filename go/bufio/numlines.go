128596c5d364d1ef87ab2955c22c079b5b5217c6
Sergio Galvan <sergalma@gmail.com>
2015-12-13 22:52:57 +0100

    Number lines from file

---
+++ go/bufio/numlines.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	filename = flag.String("f", "", "File to read")
+	numbers  = flag.Bool("n", false, "Show line numbers")
+)
+
+func main() {
+
+	flag.Parse()
+	if flag.NFlag() < 1 {
+		fmt.Fprintf(os.Stderr, "usage:%s -f <filename>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	file, err := os.Open(*filename)
+	if err != nil {
+		log.Fatal("%s\n", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	var lines int
+
+	for scanner.Scan() {
+		if *numbers {
+			fmt.Fprintf(os.Stdout, "%d: %s\n", lines, scanner.Text())
+			lines++
+			continue
+		}
+		fmt.Fprintf(os.Stdout, "%s\n", scanner.Text())
+	}
+}
