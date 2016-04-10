482e8bcd0f362ff147c27954de0dbb6202141ce0
Sergio Galvan <sergalma@gmail.com>
2016-04-10 16:15:10 -0500

    Implement flag CLI support

--- go/testing/calculator.go
+++ go/testing/calculator.go
@@ -1,6 +1,13 @@
 package main
 
-import "errors"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 var ErrDivisionByZero = errors.New("Division by zero.")
 
@@ -20,4 +27,45 @@ func DivNumbers(a, b int) (int, error) {
 		return 0, ErrDivisionByZero
 	}
 	return a / b, nil
+}
+
+func usage() {
+	var help = `calculator: [OPTIONS] arg1 arg2
+ -a:	Addition.
+ -r:	Substraction.
+ -d:	Division.
+	`
+
+	fmt.Fprintf(os.Stderr, "%s\n", help)
+	os.Exit(1)
+}
+func main() {
+	var a, b int
+	flag.Usage = usage
+	addition := flag.Bool("a", false, "")
+	division := flag.Bool("d", false, "")
+	substraction := flag.Bool("s", false, "")
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		usage()
+	}
+
+	aStr := flag.Args()[0]
+	bStr := flag.Args()[1]
+	a, _ = strconv.Atoi(aStr)
+	b, _ = strconv.Atoi(bStr)
+
+	switch {
+	case *addition:
+		fmt.Printf("Result addition: %d\n", AddNumbers(a, b))
+	case *division:
+		result, err := DivNumbers(a, b)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Result division: %d\n", result)
+	case *substraction:
+		fmt.Printf("Result subsraction: %d\n", RestNumbers(a, b))
+	}
 }
