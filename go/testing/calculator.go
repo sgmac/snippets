b82deeaed4fb9ef6b3abf6bd512ff52a62af5ed9
Sergio Galvan <sergalma@gmail.com>
2016-04-10 15:51:27 -0500

    Feature division done

--- go/testing/calculator.go
+++ go/testing/calculator.go
@@ -1,5 +1,9 @@
 package main
 
+import "errors"
+
+var ErrDivisionByZero = errors.New("Division by zero.")
+
 // AddNumbers takes 2 numbers and returns the sum.
 func AddNumbers(a, b int) int {
 	return a + b
@@ -8,4 +12,12 @@ func AddNumbers(a, b int) int {
 // AddNumbers takes 2 numbers and returns the difference.
 func RestNumbers(a, b int) int {
 	return a - b
+}
+
+// DivNumbers takes 2 numbers and performs integer division.
+func DivNumbers(a, b int) (int, error) {
+	if b == 0 {
+		return 0, ErrDivisionByZero
+	}
+	return a / b, nil
 }
