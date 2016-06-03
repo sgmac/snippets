a74135e107e419de7ffee1adc2535fd63757166b
Sergio Galvan <sergalma@gmail.com>
2016-06-03 17:59:41 -0500

    gitconfig extracts url from .git/config

---
+++ go/cli/gitconfig/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+var ErrNotDirectoryFound = errors.New(".git not found, probably not the root repository.")
+
+func main() {
+	var path string
+	flag.StringVar(&path, "p", ".", "Path for git base projects.")
+	flag.Parse()
+
+	gitconfig, err := walkGitConfig(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	re := regexp.MustCompile("url = (.*)")
+	match := re.FindStringSubmatch(gitconfig)
+	fmt.Println(match[1])
+}
+
+// find the gitconfig for a given repo and gets the url.
+func walkGitConfig(repoPath string) (string, error) {
+	repoPath, err := filepath.Abs(repoPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gitdir := path.Join(repoPath, ".git")
+	config := path.Join(gitdir, "config")
+	data, err := ioutil.ReadFile(config)
+	if err != nil {
+		errStr := err.Error()
+		if strings.Contains(errStr, "file or directory") {
+			err = ErrNotDirectoryFound
+		}
+		return "", err
+	}
+	return string(data), nil
+}
